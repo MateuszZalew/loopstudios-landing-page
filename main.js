b132d4bd7d3d90715f0807a6b0b926a9ddc57bb1
zalesiu <zalewmat31@gmail.com>
2024-09-03 14:22:35 +0200

    feat: move scripts to main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+  const sidebar = document.querySelector(".mobile-nav");
+  sidebar.style.display = "flex";
+}
+function hideSidebar() {
+  const sidebar = document.querySelector(".mobile-nav");
+  sidebar.style.display = "none";
+}
